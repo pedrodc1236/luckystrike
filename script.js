bc2de5b9bfa520337c9867a13964c47c41dc2a17
Alexandre <alexandreeduardolemes@gmail.com>
2022-02-10 18:11:53 -0300

    Estrutura inicial

---
+++ script.js
@@ -0,0 +1,7 @@
+const api = async (nomePokemon) => {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}/`);
+  const data = response.json();
+  return data;
+};
+
+console.log(api('pikachu'));
