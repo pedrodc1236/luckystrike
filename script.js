8a8805acfcb641e3cdd5ae350a6bbc220881c845
Douglas Aguiar <daguiaralcantara@gmail.com>
2022-02-11 01:52:22 -0300

    Um pouco de funcionalidade.

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+const input = document.querySelector('#time-name');
+const timeName = document.querySelector('#name-team');
+
+const changeName = () => {
+  timeName.innerText = input.value;
+}
+
 const api = async (nomePokemon) => {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nomePokemon}/`);
+  const response = await fetch(`${url}${nomePokemon}/`);
   const data = await response.json();
   /* return data; */
   console.log(data);
 };
 
-window.onload = api('pikachu') ;
+const randomFunction = async () => {
+  let counter = 0;
+  while (counter < 6) {
+    const id = Math.floor((Math.random() * 413) + 1);
+    const response = await fetch(`${url}${id}/`);
+    const data = response.json();
+    counter += 1;
+  }
+}
+
+window.onload = () => {
+  input.addEventListener('input', changeName);
+};
