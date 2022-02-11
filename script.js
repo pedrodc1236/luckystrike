c93caae6b7094beafa3d97cca2a9d96bb978b2a7
Alexandre <alexandreeduardolemes@gmail.com>
2022-02-11 17:53:06 -0300

    Continuando JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const url = 'https://pokeapi.co/api/v2/pokemon/';
+const urlAll = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=1118';
+const urlSimple = 'https://pokeapi.co/api/v2/pokemon/';
 const input = document.querySelector('#time-name');
 const timeName = document.querySelector('#name-team');
 
@@ -13,10 +14,31 @@ const api = async (nomePokemon) => {
   console.log(data);
 };
 
+const allPokemons = async () => {
+  const response = await fetch(urlAll);
+  const data = await response.json();
+  return data.results;
+  /* console.log(data.results); */
+};
+
+const pokeNames = async () => {
+  const fun = await allPokemons();
+  return fun.map((names) => names.name);
+  /* console.log(fun.map((names) => names.name)); */
+};
+
+const toUpper = async () => {
+  const names = await pokeNames();
+  console.log(names.forEach((name) => name[0].toUpperCase()))
+}
+
+toUpper();
+
+
 const randomFunction = async () => {
   let counter = 0;
   while (counter < 6) {
-    const id = Math.floor((Math.random() * 413) + 1);
+    const id = Math.floor((Math.random() * 1098) + 1);
     const response = await fetch(`${url}${id}/`);
     const data = response.json();
     counter += 1;
@@ -41,18 +63,20 @@ window.onload = () => {
 /* Modal */
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("card1");
+let cards = document.querySelectorAll(".card-pokemon");
+cards.forEach((card) => {
+  card.onclick = function() {
+    modal.style.display = "block";
+  }
+});
 
 // Get the <span> element that closes the modal
-var span = document.querySelector(".close");
+let span = document.querySelector(".close");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
